a9492be395f016a8c9e2a7fd2fbde35222245b32
Dennis Lord <lorddennis1006@gmail.com>
2018-03-26 19:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ function destructivelyRemoveFirstKitten(){kittens.shift("Milo"); return kittens}
 function appendKitten(name){var newKittens = [...kittens,name];return newKittens}
 function prependKitten(name) {var newKittens = [name, ...kittens];return newKittens;}
 function removeLastKitten() {var newKittens = kittens.slice(0, kittens.length - 1);return newKittens}
+function removeFirstKitten(){var newKittens = kittens.slice(1);return newKittens}
