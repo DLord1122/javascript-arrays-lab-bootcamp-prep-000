63490318c73671f4210c0ede82e38e44d505eea5
Dennis Lord <lorddennis1006@gmail.com>
2018-03-26 19:29:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){kittens.push(name); return kittens}
-function destructivelyPrependKitten(name)
+function destructivelyPrependKitten(name){kittens.unshift(name); return kittens}
