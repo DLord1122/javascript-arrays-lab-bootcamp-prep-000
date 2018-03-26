378e02e1df674f80c82fcc88643401dd80458546
Dennis Lord <lorddennis1006@gmail.com>
2018-03-26 19:34:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name){kittens.push(name); return kittens}
 function destructivelyPrependKitten(name){kittens.unshift(name); return kittens}
 function destructivelyRemoveLastKitten(){kittens.pop("Garfield"); return kittens}
-function destructivelyRemoveFirstKitten(){kittens.unshift("Milo"); return kittens}
+function destructivelyRemoveFirstKitten(){kittens.shift("Milo"); return kittens}
